da5704249b8c47a18ea7a637719cdf5a2902c689
YashMarakana1901 <yash.marakana1901@gmail.com>
2024-03-17 17:36:12 +0530

    tabbing added

--- src/components/business-works-tabs/BusinessWorksTabs.tsx
+++ src/components/business-works-tabs/BusinessWorksTabs.tsx
@@ -21,20 +21,50 @@ const BusinessWorksTabs = () => {
           className={`tabs ${getActiveClass(1, "active-tabs")}`}
           onClick={() => toggleTab(1)}
         >
-          Tab 1
+         branding
         </li>
         <li
           className={`tabs ${getActiveClass(2, "active-tabs")}`}
           onClick={() => toggleTab(2)}
         >
-          Tab 2
+          landing pages
         </li>
         <li
           className={`tabs ${getActiveClass(3, "active-tabs")}`}
           onClick={() => toggleTab(3)}
         >
-          Tab 3
+         product pages
         </li>
+         <li
+          className={`tabs ${getActiveClass(4, "active-tabs")}`}
+          onClick={() => toggleTab(4)}
+        >
+        social media
+        </li>
+         <li
+          className={`tabs ${getActiveClass(5, "active-tabs")}`}
+          onClick={() => toggleTab(5)}
+        >
+        packaging
+        </li>
+         <li
+          className={`tabs ${getActiveClass(6, "active-tabs")}`}
+          onClick={() => toggleTab(6)}
+        >
+         blog posts
+        </li>
+         <li
+          className={`tabs ${getActiveClass(7, "active-tabs")}`}
+          onClick={() => toggleTab(7)}
+        >
+         emails
+        </li>
+         <li
+          className={`tabs ${getActiveClass(8, "active-tabs")}`}
+          onClick={() => toggleTab(8)}
+        >
+         posters/covers
+        </li> 
       </ul>
       <div className="content-container">
         <div className={`content ${getActiveClass(1, "active-content")}`}>
@@ -44,6 +74,21 @@ const BusinessWorksTabs = () => {
           <h2>Ipsum</h2>
         </div>
         <div className={`content ${getActiveClass(3, "active-content")}`}>
+          <h2>Dolor</h2>
+        </div>
+        <div className={`content ${getActiveClass(4, "active-content")}`}>
+          <h2>Dolor</h2>
+        </div>
+        <div className={`content ${getActiveClass(5, "active-content")}`}>
+          <h2>Dolor</h2>
+        </div>
+        <div className={`content ${getActiveClass(6, "active-content")}`}>
+          <h2>Dolor</h2>
+        </div>
+        <div className={`content ${getActiveClass(7, "active-content")}`}>
+          <h2>Dolor</h2>
+        </div>
+        <div className={`content ${getActiveClass(8, "active-content")}`}>
           <h2>Dolor</h2>
         </div>
       </div>
